5f580f832c7e314ffa90c708c125f13303c298ce
elodiemartin <elodie.m@codeur.online>
2018-10-09 13:38:21 +0200

    modifs

--- app.js
+++ app.js
@@ -1,31 +1,26 @@
-let random = getRndInteger (0, 100); 
+let random = Math.floor(Math.random() * 101); 
 
-var number = document.querySelector("#number.value");
+let number;
 
-let button = document.querySelector("#button");
+const button = document.querySelector("#button");
 
 let test = document.querySelector("#test");
 
 test.innerHTML = random;
 
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min ) ) + min;
-}
-
-
 button.addEventListener("click" , function(){
+    number = document.querySelector("#number");
+    
+    if (number.value == "") {
+        window.alert("vide");
 
-    if(number === random){
-    window.alert("GG")
+    } else if (number.value < random) {
+        window.alert("Trop bas");
+
+    } else if (number.value > random) {
+        window.alert("Trop haut");
+
+    } else if (number.value == random) {
+        window.alert("GG");
     }
-        else if (number < random){
-        window.alert("Trop haut")
-        }
-        else if(number > random){
-            window.alert("Trop bas")
-            }   
-}
-)
-
-console.log(number)
-console.log(random);
+})
