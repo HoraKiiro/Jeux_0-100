852c637ec4b95350b632c7425369ac58ae24f333
Killian <killian.l@codeur.online>
2018-10-09 14:33:45 +0200

    [HTML] [JS]
    Fonctionnement basique du jeu

--- app.js
+++ app.js
@@ -1,26 +1,58 @@
-let random = Math.floor(Math.random() * 101); 
+let random = Math.floor(Math.random() * (100 - 1 ) ) + 1;
 
 let number;
 
 const button = document.querySelector("#button");
 
+const reset = document.querySelector("#reset");
+
 let test = document.querySelector("#test");
+
+let compteur = document.querySelector("#compteur");
+
+let vie = 10;
 
 test.innerHTML = random;
 
-button.addEventListener("click" , function(){
+
+
+
+button.addEventListener("click", function () {
     number = document.querySelector("#number");
-    
     if (number.value == "") {
         window.alert("vide");
-
-    } else if (number.value < random) {
-        window.alert("Trop bas");
-
-    } else if (number.value > random) {
-        window.alert("Trop haut");
-
-    } else if (number.value == random) {
-        window.alert("GG");
     }
+        else if (vie === 0){
+
+        }
+
+        else {
+
+        if (number.value < random) {
+            window.alert("Trop bas");
+            vie = vie-1;
+
+        } else if (number.value > random) {
+            window.alert("Trop haut");
+            vie = vie-1;
+
+        } else{
+            window.alert("GG");
+            vie = "";
+        }
+    }
+    compteur.innerHTML = vie;
 })
+
+reset.addEventListener("click", function() {
+    vie = 10;
+    random = Math.floor(Math.random() * (100 - 1 ) ) + 1;
+    compteur.innerHTML = vie;
+    test.innerHTML = random;
+})
+
+
+
+console.log(compteur);
+console.log(number);
+console.log(random);
